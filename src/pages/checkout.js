95030fbd2feae85b3ec594e667bbddfea119185f
Conrad <CGKuanYew.1990@gmail.com>
2021-12-11 16:45:16 +0800

    day 3 complete

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -2,13 +2,14 @@ import React from 'react'
 import Header from '../components/Header'
 import Image from "next/image"
 import CheckoutProduct from '../components/CheckoutProduct'
-import {selectItems} from "../slices/basketSlice"
+import {selectItems, selectTotal} from "../slices/basketSlice"
 import {useSelector} from "react-redux"
 import CurrencyFormat from 'react-currency-format';
 import {useSession} from "next-auth/react";
 
 function Checkout() {
     const items = useSelector(selectItems);
+    const total = useSelector(selectTotal);
     const {data:session} = useSession();
    
     return (
@@ -48,23 +49,25 @@ function Checkout() {
             </div>
         </div>
 
-            <div>
+            <div className="flex flex-col bg-white p-10 shadow-md">
                 {items.length> 0 && (
                     <>
                     <h2>
                         Subtotal ({items.length}items):
                         <span className="font-bold">
 
-                        {/* <CurrencyFormat 
+                        <CurrencyFormat 
                         className="font-bold "
-                        value={price} 
+                        value={total} 
                         displayType={'text'}
                         thousandSeparator={true}
-                        prefix={'MYR'} /> */}
+                        prefix={'MYR'} />
                         </span> 
                     </h2>
 
-                        <button className={`button mt-2 ${!session && 'from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed'}`}>
+                        <button 
+                        disabled={!session}
+                        className={`buttonDesign mt-2 ${!session && 'from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed'}`}>
                             {!session?"Sign in to checkout" : "Proceed to checkout"}
                         </button>
                     </>
