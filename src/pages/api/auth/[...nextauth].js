836c47f48f07d905f36c06c43bf081cf96cdb585
Conrad <CGKuanYew.1990@gmail.com>
2021-12-21 16:28:45 +0800

    nextAuth update

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -9,7 +9,6 @@ export default NextAuth({
     GoogleProvider({
         clientId: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-       
     }),
 
     FacebookProvider({
